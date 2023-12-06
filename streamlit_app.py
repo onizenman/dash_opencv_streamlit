56634312b3707e598353a49921734b443d3742c2
onizenman <bujikan@gmail.com>
2023-12-05 20:33:11 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,13 @@ from PIL import Image
 from io import BytesIO
 import base64
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 
 
